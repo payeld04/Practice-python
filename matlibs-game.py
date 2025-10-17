c4fa5728993d8caae2f11127e1e38d5a55e10f51
Payel <daspayell346@gmail.com>
2025-10-17 23:50:58 +0530

    initial commit

---
+++ matlibs-game.py
@@ -0,0 +1,13 @@
+#word game where you create a story
+#by filling in blanks with random words
+
+adj1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adj2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adj3 = input("Enter an adjective (description): ")
+
+print(f"Today I went a {adj1} festivel.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}")
