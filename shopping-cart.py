5c9acc7e7355d3ee5c6dcb09d6ff9e4d3b1e19bf
Payel <daspayell346@gmail.com>
2025-10-17 23:50:22 +0530

    initial commit

---
+++ shopping-cart.py
@@ -0,0 +1,11 @@
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+
+price(total)
+
+#or
+print(f"you have bought{quantity}x{item}/s")
+print(f"your total is: {total}")
